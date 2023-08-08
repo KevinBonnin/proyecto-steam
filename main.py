3595415dc525730dacdaca0c8c9497f71895b996
KevinBonnin <kevin.bonnin65@gmail.com>
2023-08-08 01:18:13 -0300

    9

--- main.py
+++ main.py
@@ -5,41 +5,41 @@ import ast
 
 inf = pd.read_csv('steam_games.csv')
 
-inf['año'] = inf['año'].astype(str)
+inf['Year'] = inf['Year'].astype(str)
 
 app = FastAPI()
 
-@app.get("/genero/{Año}")
-def genero( Año: str ):
-    juegos = (inf[inf['año'] == Año])
+@app.get("/genero/{Year}")
+def genero( Year: str ):
+    juegos = (inf[inf['Year'] == Year])
     juegos['genres'] = juegos['genres'].apply(lambda x: ast.literal_eval(x) if pd.notnull(x) else [])
     generos_desglosados = juegos['genres'].explode()
     generos_mas_vendidos = generos_desglosados.value_counts().head(5).index.tolist()
-    return {'Los generos mas vendidos en el año': Año, 'son': generos_mas_vendidos}
+    return {'Los generos mas vendidos en el Year': Year, 'son': generos_mas_vendidos}
 
-@app.get("/juegos/{Año}")
-def juegos( Año: str ):
-    filtro = (inf[inf['año'] == Año])
+@app.get("/juegos/{Year}")
+def juegos( Year: str ):
+    filtro = (inf[inf['Year'] == Year])
     juego = filtro[['title']].values.tolist()
-    return {'Los juegos que se lanzaron en el año': Año, 'son': juego}
+    return {'Los juegos que se lanzaron en el Year': Year, 'son': juego}
 
-@app.get("/specs/{Año}")
-def specs( Año: str ):
-    juegos = (inf[inf['año'] == Año])
+@app.get("/specs/{Year}")
+def specs( Year: str ):
+    juegos = (inf[inf['Year'] == Year])
     juegos['specs'] = juegos['specs'].apply(lambda x: ast.literal_eval(x) if pd.notnull(x) else [])
     especificaciones_desglosadas = juegos['specs'].explode()
     especificaciones_mas_repetidas = especificaciones_desglosadas.value_counts().head(5).index.tolist()
-    return {'Las especificaciones mas repetidas en el año': Año, 'son': especificaciones_mas_repetidas}
+    return {'Las especificaciones mas repetidas en el Year': Year, 'son': especificaciones_mas_repetidas}
 
-@app.get("/earlyacces/{Año}")
-def earlyacces( Año: str ):
-    juegos_early_access = inf[(inf['año'] == Año) & (inf['early_access'] == True)]
+@app.get("/earlyacces/{Year}")
+def earlyacces( Year: str ):
+    juegos_early_access = inf[(inf['Year'] == Year) & (inf['early_access'] == True)]
     cantidad_juegos_early_access = juegos_early_access.shape[0]
-    return {'La cantidad de juegos lanzados en el año': Año, 'es de': cantidad_juegos_early_access}
+    return {'La cantidad de juegos lanzados en el Year': Year, 'es de': cantidad_juegos_early_access}
 
-@app.get("/sentiment/{Año}")
-def sentiment( Año: str ):
-    data_filtrada = inf[inf['año'] == Año]
+@app.get("/sentiment/{Year}")
+def sentiment( Year: str ):
+    data_filtrada = inf[inf['Year'] == Year]
     data_filtrada['sentiment'] = data_filtrada['sentiment'].astype(str)
     data_filtrada = data_filtrada[~data_filtrada['sentiment'].str.contains(r'\d+ user reviews', case=False) & ~data_filtrada['sentiment'].isnull()]
     conteo_sentimiento = data_filtrada['sentiment'].value_counts()
@@ -47,8 +47,8 @@ def sentiment( Año: str ):
         del conteo_sentimiento['nan']
     return conteo_sentimiento.to_dict()
 
-@app.get("/metascore/{Año}")
-def metascore( Año: str ):
-    top_juegos_metascore = inf[inf['año'] == Año].nlargest(5, 'metascore')
+@app.get("/metascore/{Year}")
+def metascore( Year: str ):
+    top_juegos_metascore = inf[inf['Year'] == Year].nlargest(5, 'metascore')
     juegoss = top_juegos_metascore[['title', 'metascore']]
     return {'Los juegos con mayor metascore y sus respectivos valores son': juegoss}
